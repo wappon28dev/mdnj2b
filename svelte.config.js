ad7c52b8890018e40ff8802c0046f6f9d6b634f8
wappon_28_dev <86721991+wappon-28-dev@users.noreply.github.com>
2022-06-14 03:56:49 +0900

    config_static_page

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,21 @@
-import adapter from "@sveltejs/adapter-auto";
-import preprocess from "svelte-preprocess";
+import sveltePreprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
+export default {
+  preprocess: sveltePreprocess(),
 
   kit: {
-    vite: {
-      plugins: [],
+    adapter: adapter({
+      // default options are shown
+      pages: "public",
+      assets: "public",
+      fallback: null,
+      precompress: false,
+    }),
+
+    prerender: {
+      // This can be false if you're using a fallback (i.e. SPA mode)
+      default: true,
     },
   },
 };
-
-export default config;
