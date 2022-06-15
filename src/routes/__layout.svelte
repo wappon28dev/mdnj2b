89a76e198b4e50b9d628493d6cb1346bc9734e71
wappon_28_dev <86721991+wappon-28-dev@users.noreply.github.com>
2022-06-15 23:58:55 +0900

    added_app_drawer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
-  import Button from "@smui/button";
   import type { TopAppBarComponentDev } from "@smui/top-app-bar";
-
+  import Drawer, {
+    AppContent,
+    Content,
+    Title as DrawerTitle,
+    Header,
+    Subtitle,
+  } from "@smui/drawer";
+  import Button, { Label } from "@smui/button";
+  import List, { Item, Text } from "@smui/list";
   import TopAppBar, {
     Row,
     Section,
@@ -9,9 +16,14 @@
     AutoAdjust,
   } from "@smui/top-app-bar";
   import IconButton from "@smui/icon-button";
-  import { Label } from "@smui/common";
 
   let topAppBar: TopAppBarComponentDev;
+
+  let open = true;
+  let active = "Gray Kittens";
+  function setActive(value: string) {
+    active = value;
+  }
 
   let lightTheme =
     typeof window === "undefined" ||
@@ -34,7 +46,9 @@
 <TopAppBar bind:this={topAppBar} variant="short">
   <Row>
     <Section>
-      <IconButton class="material-icons">menu</IconButton>
+      <IconButton class="material-icons" on:click={() => (open = !open)}
+        >menu</IconButton
+      >
       <Title>mdnj2b</Title>
     </Section>
     <Section align="end" toolbar>
@@ -44,10 +58,71 @@
     </Section>
   </Row>
 </TopAppBar>
-<AutoAdjust {topAppBar} style="display: flex; justify-content: space-between;">
-  <div class="container"><slot /></div>
-  <div class="container" />
-</AutoAdjust>
+
+<div class="drawer-container">
+  <Drawer variant="modal" fixed={false} bind:open>
+    <Header>
+      <DrawerTitle>Super Drawer</DrawerTitle>
+      <Subtitle>It's the best drawer.</Subtitle>
+    </Header>
+    <Content>
+      <List>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive("Gray Kittens")}
+          activated={active === "Gray Kittens"}
+        >
+          <Text>Gray Kittens</Text>
+        </Item>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive("A Space Rocket")}
+          activated={active === "A Space Rocket"}
+        >
+          <Text>A Space Rocket</Text>
+        </Item>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive("100 Pounds of Gravel")}
+          activated={active === "100 Pounds of Gravel"}
+        >
+          <Text>100 Pounds of Gravel</Text>
+        </Item>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive("All of the Shrimp")}
+          activated={active === "All of the Shrimp"}
+        >
+          <Text>All of the Shrimp</Text>
+        </Item>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive("A Planet with a Mall")}
+          activated={active === "A Planet with a Mall"}
+        >
+          <Text>A Planet with a Mall</Text>
+        </Item>
+      </List>
+    </Content>
+  </Drawer>
+
+  <AutoAdjust
+    {topAppBar}
+    style="display: flex; justify-content: space-between;"
+  >
+    <AppContent class="app-content">
+      <main class="main-content">
+        <Button on:click={() => (open = !open)}
+          ><Label>Toggle Drawer</Label></Button
+        >
+        <br />
+        <pre class="status">Active: {active}</pre>
+        <p>{open}</p>
+        <slot />
+      </main>
+    </AppContent>
+  </AutoAdjust>
+</div>
 
 <style>
   /* Hide everything above this component. */
@@ -58,5 +133,27 @@
     height: auto !important;
     width: auto !important;
     position: static !important;
+  }
+
+  .drawer-container {
+    position: relative;
+    display: flex;
+    height: max-content;
+    max-width: 600px;
+    min-width: max-content;
+    overflow: hidden;
+    z-index: 0;
+  }
+  * :global(.app-content) {
+    flex: auto;
+    overflow: auto;
+    position: relative;
+    flex-grow: 1;
+  }
+  .main-content {
+    overflow: auto;
+    padding: 16px;
+    height: 100%;
+    box-sizing: border-box;
   }
 </style>
